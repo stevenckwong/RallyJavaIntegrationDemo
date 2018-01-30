35e9c96b3e1fc7ebaf884bffd9ceebef0d5d0a15
steven <steven@stevens-mbp.ca.com>
2018-01-30 11:09:31 +0900

    US198: Integrate with Jenkins for Orchestration
    Task-Url: https://rally1.rallydev.com/#/190648219896u/detail/userstory/193975523988

--- runTest.sh
+++ runTest.sh
@@ -1,2 +1,2 @@
-java -classpath ./WebContent/WEB-INF/lib/*:./temp/WEB-INF/classes -Dwebdriver.chrome.driver="/Users/steven/Applications/chromedriver" org.junit.runner.JUnitCore com.stevenckwong.rallyintegration.TestLoginGetNameService
+java -classpath ./WebContent/WEB-INF/lib/*:./temp/WEB-INF/classes -Dwebdriver.chrome.driver="/opt/chromedriver" org.junit.runner.JUnitCore com.stevenckwong.rallyintegration.TestLoginGetNameService
 
